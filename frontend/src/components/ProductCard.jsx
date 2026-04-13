5cab6016e476a845749302247d7267fe5805f17f
ASW-programming <adamwinkler7@gmail.com>
2026-04-13 15:31:01 +0200

    Initial Product Card

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+function ProductCard() {
+	return (
+		<div>
+			<img
+				src="https://assets.axfood.se/image/upload/f_auto,t_500/07340083463907_C1L1_s04"
+				alt=""
+				style={{ width: "100px", height: "100px" }}
+			/>
+			<h2>Mackor</h2>
+			<p style={{ textDecoration: "bold" }}>Garant</p>
+			<p>
+				Mustigt rågbröd med surdeg. Rikt på fibrer med 64% fullkorn. 340
+				gram.
+			</p>
+		</div>
+	);
+}
+
+export default ProductCard;
