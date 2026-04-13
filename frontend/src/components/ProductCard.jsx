ac035c8a9c1b5a0645d2392cd294ec72b6082481
ASW-programming <adamwinkler7@gmail.com>
2026-04-13 18:13:21 +0200

    Added test object and variables to text

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,17 +1,34 @@
 function ProductCard() {
+	const testItem = {
+		name: "Mackor",
+		brand: "Garant",
+		price: 49,
+		description:
+			"Mustigt rågbröd med surdeg. \nRikt på fibrer med 64% fullkorn. 340 gram. \nUrsprung Estland. \nIngredienser: rågmjöl och vetemjöl.",
+		category: "Bröd",
+		stock: 25,
+		imageUrl:
+			"https://assets.axfood.se/image/upload/f_auto,t_500/07340083463907_C1L1_s04",
+	};
+
 	return (
-		<div>
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				border: "solid black 1px",
+			}}>
 			<img
-				src="https://assets.axfood.se/image/upload/f_auto,t_500/07340083463907_C1L1_s04"
+				src={testItem.imageUrl}
 				alt=""
 				style={{ width: "100px", height: "100px" }}
 			/>
-			<h2>Mackor</h2>
-			<p style={{ textDecoration: "bold" }}>Garant</p>
-			<p>
-				Mustigt rågbröd med surdeg. Rikt på fibrer med 64% fullkorn. 340
-				gram.
-			</p>
+			<span style={{ fontWeight: "bold" }}>{testItem.name}</span>
+			<span style={{ textDecoration: "bold" }}>{testItem.brand}</span>
+			<span style={{ whiteSpace: "pre-line" }}>
+				{testItem.description.slice(0, 29)}
+			</span>
+			<span>{`${testItem.price} kr`}</span>
 		</div>
 	);
 }
