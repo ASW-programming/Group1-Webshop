7637fd4b352fc86658728ad5aeee13a36cb63c7c
ASW-programming <adamwinkler7@gmail.com>
2026-05-01 19:15:19 +0200

    Added sorting based on order ID, date, customer and total price with visual cue

--- frontend/src/components/OrderHistory.jsx
+++ frontend/src/components/OrderHistory.jsx
@@ -2,10 +2,14 @@ import ItemButton from "./ItemButton";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "../utils/calls.js";
-import { ReturnIcon, HomeIcon } from "../assets/Icons.jsx";
+import { ReturnIcon, HomeIcon, SortIcon } from "../assets/Icons.jsx";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function OrderHistory() {
+	const [sortCategory, setSortCategory] = useState("orderID");
+	const [sortAscending, setSortAscending] = useState(true);
+
 	const {
 		data: orders,
 		isLoading,
@@ -17,6 +21,12 @@ function OrderHistory() {
 
 	const formatDate = (createdAt) =>
 		createdAt ? new Date(createdAt).toLocaleString("sv-SE") : "Okänt datum";
+
+	const handleSort = (key) => {
+		if (sortCategory !== key) setSortAscending(true);
+		else setSortAscending((prev) => !prev);
+		setSortCategory(key);
+	};
 
 	if (isLoading) return <p>Loading</p>;
 	if (isError) return <p>Error</p>;
@@ -37,64 +47,175 @@ function OrderHistory() {
 
 			<table className="orderTable">
 				<thead>
-					<tr>
-						<th>Order #</th>
-						<th>Datum</th>
-						<th>Kund</th>
+					<tr className="orderCategories">
+						<th
+							className={
+								sortCategory === "orderID"
+									? "chosen"
+									: "notChosen"
+							}
+							onClick={() => handleSort("orderID")}>
+							<SortIcon
+								style={{
+									visibility:
+										sortCategory === "orderID"
+											? "visible"
+											: "hidden",
+								}}
+								transform={
+									sortAscending ? "rotate(180, 8.5, 5.5)" : ""
+								}
+							/>{" "}
+							Order #
+						</th>
+						<th
+							className={
+								sortCategory === "createdAt"
+									? "chosen"
+									: "notChosen"
+							}
+							onClick={() => handleSort("createdAt")}>
+							<SortIcon
+								style={{
+									visibility:
+										sortCategory === "createdAt"
+											? "visible"
+											: "hidden",
+								}}
+								transform={
+									sortAscending ? "rotate(180, 8.5, 5.5)" : ""
+								}
+							/>
+							Datum
+						</th>
+						<th
+							className={
+								sortCategory === "customer"
+									? "chosen"
+									: "notChosen"
+							}
+							onClick={() => handleSort("customer")}>
+							<SortIcon
+								style={{
+									visibility:
+										sortCategory === "customer"
+											? "visible"
+											: "hidden",
+								}}
+								transform={
+									sortAscending ? "rotate(180, 8.5, 5.5)" : ""
+								}
+							/>
+							Kund
+						</th>
 						<th>Produkt</th>
 						<th>Antal</th>
 						<th>Pris</th>
-						<th>Total</th>
+						<th
+							className={
+								sortCategory === "totalPrice"
+									? "chosen"
+									: "notChosen"
+							}
+							onClick={() => handleSort("totalPrice")}>
+							<SortIcon
+								style={{
+									visibility:
+										sortCategory === "totalPrice"
+											? "visible"
+											: "hidden",
+								}}
+								transform={
+									sortAscending ? "rotate(180, 8.5, 5.5)" : ""
+								}
+							/>
+							Total
+						</th>
 					</tr>
 				</thead>
 				<tbody>
-					{orders?.map((o) =>
-						o.items.map((i, index) => (
-							<tr key={`${o.id}-${i.id}`} className="orderRow">
-								{index === 0 && (
-									<td rowSpan={o.items.length}>
-										{o.orderIDFormatted ||
-											"Order ID Saknas"}
-									</td>
-								)}
+					{orders
+						?.slice()
+						.sort((a, b) => {
+							const choiceA = a[sortCategory];
+							const choiceB = b[sortCategory];
 
-								{index === 0 && (
-									<td rowSpan={o.items.length}>
-										{formatDate(o.createdAt)}
-									</td>
-								)}
+							if (!sortCategory) return 0;
 
-								{index === 0 && (
-									<td
-										rowSpan={o.items.length}
-										className="orderCustomer">
-										{o.customer}
+							// Checks if Choice A is a string and not a date. If not a date then isNaN = true and return to sort String
+							if (
+								typeof choiceA === "string" &&
+								isNaN(Date.parse(choiceA))
+							) {
+								// Checks if A is greater than B with support of Swedish Letters
+								return sortAscending
+									? choiceA.localeCompare(choiceB, "sv-SE")
+									: choiceB.localeCompare(choiceA, "sv-SE");
+							}
+
+							// Checks the date if its newer or older.
+							if (typeof choiceA === "string") {
+								return sortAscending
+									? new Date(choiceA) - new Date(choiceB)
+									: new Date(choiceB) - new Date(choiceA);
+							}
+
+							// Sorts numbers
+							return sortAscending
+								? choiceA - choiceB
+								: choiceB - choiceA;
+						})
+						.map((o) =>
+							o.items.map((i, index) => (
+								<tr
+									key={`${o.id}-${i.id}`}
+									className="orderRow">
+									{index === 0 && (
+										<td rowSpan={o.items.length}>
+											{o.orderIDFormatted ||
+												"Order ID Saknas"}
+										</td>
+									)}
+
+									{index === 0 && (
+										<td rowSpan={o.items.length}>
+											{formatDate(o.createdAt)}
+										</td>
+									)}
+
+									{index === 0 && (
+										<td
+											rowSpan={o.items.length}
+											className="orderCustomer">
+											{o.customer}
+										</td>
+									)}
+									<td className="orderImage orderName">
+										<img
+											src={i.imageUrl}
+											className="orderItemImage"
+										/>
+										{i.name}
 									</td>
-								)}
-								<td className="orderImage orderName">
-									<img
-										src={i.imageUrl}
-										className="orderItemImage"
-									/>
-									{i.name}
-								</td>
-								<td className="orderQuantity">
-									{i.quantity} st
-								</td>
-								<td className="orderPrice">
-									{i.reducedPrice ? i.reducedPrice : i.price}{" "}
-									kr
-								</td>
-								{index === 0 && (
-									<td
-										rowSpan={o.items.length}
-										className="orderTotal">
-										{o.price} kr
+									<td className="orderQuantity">
+										{i.quantity} st
 									</td>
-								)}
-							</tr>
-						)),
-					)}
+									<td className="orderPrice">
+										{i.reducedPrice
+											? i.reducedPrice
+											: i.price}{" "}
+										kr
+									</td>
+									{index === 0 && (
+										<td
+											rowSpan={o.items.length}
+											className="orderTotal">
+											{o.totalPrice} kr
+										</td>
+									)}
+								</tr>
+							)),
+						)}
 				</tbody>
 			</table>
 		</div>
